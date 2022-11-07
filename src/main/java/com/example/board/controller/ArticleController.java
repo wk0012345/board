1c0865b05d95c940bf5fd4b11829082c155b67f1
wk0012345 <wk0012345@naver.com>
2022-11-07 23:36:32 +0900

    #16 - view test 정의

---
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * articles
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
