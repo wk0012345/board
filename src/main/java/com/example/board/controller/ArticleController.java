ede3dc282606c81c5bee85092862e222a2d257b3
wk0012345 <wk0012345@naver.com>
2022-11-10 00:21:55 +0900

    #16 - 게시글 뷰 기본 골격 작성

--- src/main/java/com/example/board/controller/ArticleController.java
+++ src/main/java/com/example/board/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,5 +19,14 @@ public class ArticleController {
 
         modelMap.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable long articleId, ModelMap modelMap) {
+
+        modelMap.addAttribute("article", null);
+        modelMap.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
