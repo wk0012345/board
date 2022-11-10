22a339ea39f877c39b5ee08d20af0b6376616daa
wk0012345 <wk0012345@naver.com>
2022-11-10 23:56:32 +0900

    #16 - login controller test

--- src/test/java/com/example/board/controller/ArticleControllerTest.java
+++ src/test/java/com/example/board/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.board.controller;
 
+import com.example.board.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,6 +14,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @DisplayName("View controller articles")
+@Import(SecurityConfig.class)
 @WebMvcTest
 class ArticleControllerTest {
     private final MockMvc mockMvc;
---
+++ src/test/java/com/example/board/controller/AuthControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@DisplayName("뷰 컨트롤러 인증")
+@WebMvcTest
+public class AuthControllerTest {
+    private final MockMvc mockMvc;
+
+    AuthControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("[view] [GET] 로그인 페이지")
+    @Test
+    public void givenNothing_whenTryingToLoggingIn_thenReturnsLoginView() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
