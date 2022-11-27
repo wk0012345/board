aef0e10069025915e086fde725a9ced603a97100
wk0012345 <wk0012345@naver.com>
2022-11-27 23:27:53 +0900

    leetcode

--- src/main/java/com/example/board/domain/ArticleComment.java
+++ src/main/java/com/example/board/domain/ArticleComment.java
@@ -5,10 +5,12 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -16,17 +18,6 @@ import java.util.Objects;
 })
 @Entity
 public class ArticleComment extends AuditingFields {
-    protected ArticleComment() {
-    }
-
-    private ArticleComment(Article article, String content) {
-        this.article = article;
-        this.content = content;
-    }
-
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,22 +25,55 @@ public class ArticleComment extends AuditingFields {
 
     @Setter
     @ManyToOne(optional = false)
-    private Article article; // 게새글 (ID)
+    private Article article; // 게시글 (ID)
+
+    @Setter
+    @JoinColumn(name = "userId")
+    @ManyToOne(optional = false)
+    private UserAccount userAccount; // 유저 정보 (ID)
+
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId; // 부모 댓글 ID
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
 
     @Setter
     @Column(nullable = false, length = 500)
     private String content; // 본문
 
+
+    protected ArticleComment() {
+    }
+
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
+        this.article = article;
+        this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ArticleComment that = (ArticleComment) o;
-        return id != null && Objects.equals(id, that.id);
+        if (!(o instanceof ArticleComment that)) return false;
+        return this.getId() != null && this.getId().equals(that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(this.getId());
     }
 }
---
+++ src/main/java/com/example/board/domain/type/SearchType.java
@@ -0,0 +1,9 @@
+package com.example.board.domain.type;
+
+public enum SearchType {
+    TITLE,
+    CONTENT,
+    ID,
+    NICKNAME,
+    HASHTAG
+}
